6c2d58717f9360d988356927233e55317d24b2a2
rei-kes <127288356+rei-kes@users.noreply.github.com>
2023-03-10 01:02:19 -0500

    Update HooksAndLoops.lua

--- TypicalColors2/Other/HooksAndLoops.lua
+++ TypicalColors2/Other/HooksAndLoops.lua
@@ -34,16 +34,18 @@ local OldNameCall;OldNameCall = hookmetamethod(game, "__namecall", function(...)
     local Args = {...}
     local Self = Args[1]
     if Self and typeof(Self) == "Instance" and getnamecallmethod():lower() == "fireserver" then
-        if Self.Name == "BeanBoozled" then -- do not disable this under any circumstances
+        if Self.Name == "BeanBoozled" or Self.Name == "empty" then -- literally the entire anticheat
             return wait(9e9)
 
-        elseif Self.Name == "CreateProjectile" and cameraOVERRIDE then
-            Args[3] = workSpace.CurrentCamera.CFrame.Position + lookVectorOVERRIDE * 100
-            Args[4] = CFrame.lookAt(cameraOVERRIDE, Args[3])--CFrame.lookAt(workSpace.CurrentCamera.CFrame.Position, Args[3]) * projectileData[variables.gun.Value.Name].Offset
-
+        elseif Self.Name == "CreateProjectile" then
             --Args[11] Toggles.SpoofWeaponCharge.Value and Args[2] == "Stickybomb" and 1
 
-            return OldNameCall(unpack(Args))
+            if cameraOVERRIDE then
+                Args[3] = workSpace.CurrentCamera.CFrame.Position + lookVectorOVERRIDE * 100
+                Args[4] = CFrame.lookAt(cameraOVERRIDE, Args[3])
+
+                return OldNameCall(unpack(Args))
+            end
         end
     end
     return OldNameCall(...)
@@ -59,6 +61,19 @@ end)
 -- other hooks
 
 do
+    local _function = weaponsRequire.firebullet
+    weaponsRequire.firebullet = function(...)
+        pcall(function()
+            if not Toggles.ProjectileAutoShoot.Value and projectileData[variables.gun.Value.Name] then
+                Ticks["ProjectileAimbot"](true)
+            end
+        end)
+
+        _function(...)
+    end
+end
+
+do
     local _function = getsenv(weaponsScript)["RotCamera"]
     getsenv(weaponsScript)["RotCamera"] = function(...)
         if not lookVectorOVERRIDE then
@@ -71,30 +86,30 @@ end
 
 -- loops
 
-local heartbeatFunctions = {}
+getgenv().Frames = {}
 run.Heartbeat:Connect(function()
     local success, _error = pcall(function()
-        for _,_function in heartbeatFunctions do
+        for _,_function in Frames do
             _function()
         end
     end)
     if not success then print(_error) end
 end)
-getgenv().AddHeartbeat = function(_function)
-    heartbeatFunctions[#heartbeatFunctions + 1] = _function
-end
 
-local tickFunctions = {}
+getgenv().Ticks = {}
 task.spawn(function()
     while task.wait(0.1) do
         local success, _error = pcall(function()
-            for _,_function in tickFunctions do
+            for _,_function in Ticks do
                 _function()
             end
         end)
         if not success then print(_error) end
     end
 end)
-getgenv().AddTick = function(_function)
-    tickFunctions[#tickFunctions + 1] = _function
-end
+
+    -- specific loops
+
+    Frames["Ping"] = function()
+        getgenv().ping = _stats.Network.ServerStatsItem["Data Ping"]:GetValue() / 1000
+    end
